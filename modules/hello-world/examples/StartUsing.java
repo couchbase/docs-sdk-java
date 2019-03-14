e2dfedcdcb5c97c306e3dd138d1320645a9d5dd3
Michael Nitschinger <michael@nitschinger.at>
2019-03-14 07:38:22 +0100

    Update StartUsing for alpha.1 target

---
+++ modules/hello-world/examples/StartUsing.java
@@ -0,0 +1,40 @@
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.kv.MutationResult;
+import com.couchbase.client.java.json.JsonObject;
+import com.couchbase.client.java.kv.GetResult;
+import com.couchbase.client.java.Collection;
+
+import java.util.Optional;
+
+class StartUsing {
+
+    public static void main(String... args) {
+
+// #tag::connect[]
+Cluster cluster = Cluster.connect("localhost", "username", "password");
+// #end::connect[]
+
+// #tag::bucket[]
+// get a bucket reference
+Bucket bucket = cluster.bucket("bucket-name");
+// #end::bucket[]
+
+// #tag::collection[]
+// get a collection reference
+Collection collection = bucket.defaultCollection();
+// #end::collection[]
+
+// #tag::upsert-get[]
+// Upsert Document
+MutationResult upsertResult = collection.upsert(
+    "my-document", 
+    JsonObject.create().put("name", "mike")
+);
+
+// Get Document
+Optional<GetResult> getResult = collection.get("my-document");
+// #end::upsert-get[]
+
+    }
+}
