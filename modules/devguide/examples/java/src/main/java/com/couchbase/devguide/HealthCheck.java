f3b6196bb3da71811d3ea538aef7aed4b0233a15
Matt Ingenthron <ingenthr@cep.net>
2020-04-23 22:35:03 -0700

    Migration guide English simplification.

---
+++ modules/devguide/examples/java/src/main/java/com/couchbase/devguide/HealthCheck.java
@@ -0,0 +1,29 @@
+package com.couchbase.devguide;
+
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+
+/**
+ * Example of Retrieving in Java for the Couchbase Developer Guide.
+ */
+public class Retrieving extends ConnectionBase {
+
+    @Override
+    protected void doWork() {
+        String key = "javaDevguideExampleRetrieving";
+        LOGGER.info("Getting non-existent key. Should fail..");
+        JsonDocument nonExistentDocument = bucket.get("non-exist-document");
+        if (nonExistentDocument == null) {
+            LOGGER.info("Got null for missing document, it doesn't exist!");
+        }
+
+        LOGGER.info("Upserting...");
+        Retrieving        bucket.upsert(document);
+        LOGGER.info("Getting...");
+        LOGGER.info(bucket.get(key));
+    }
+
+    public static void main(String[] args) {
+        new Retrieving().execute();
+    }
+}
