18ab91c16150279511714bb963a4b3deda03473e
Maria Shodunke <mariashow2001@gmail.com>
2021-02-23 15:40:02 +0000

    Ensure Queries.java can actually run

--- modules/howtos/examples/KvOperations.java
+++ modules/howtos/examples/KvOperations.java
@@ -38,21 +38,21 @@ public class KvOperations {
     Cluster cluster = Cluster.connect("127.0.0.1", "Administrator", "password");
 
     Bucket bucket = cluster.bucket("travel-sample");
-    Scope defaultScope = bucket.scope("_default");
-    Collection defaultCollection = defaultScope.collection("_default");
+    Scope scope = bucket.scope("_default");
+    Collection collection = scope.collection("_default");
 
     JsonObject json = JsonObject.create().put("title", "My Blog Post").put("author", "mike");
 
     // tag::apis[]
-    AsyncCollection asyncDefaultCollection = defaultCollection.async();
-    ReactiveCollection reactiveDefaultCollection = defaultCollection.reactive();
+    AsyncCollection asynccollection = collection.async();
+    ReactiveCollection reactivecollection = collection.reactive();
     // end::apis[]
 
     {
       // tag::upsert[]
       JsonObject content = JsonObject.create().put("author", "mike").put("title", "My Blog Post 1");
 
-      MutationResult result = defaultCollection.upsert("document-key", content);
+      MutationResult result = collection.upsert("document-key", content);
       // end::upsert[]
     }
 
@@ -60,7 +60,7 @@ public class KvOperations {
       // tag::insert[]
       try {
         JsonObject content = JsonObject.create().put("title", "My Blog Post 2");
-        MutationResult insertResult = defaultCollection.insert("document-key2", content);
+        MutationResult insertResult = collection.insert("document-key2", content);
       } catch (DocumentExistsException ex) {
         System.err.println("The document already exists!");
       } catch (CouchbaseException ex) {
@@ -72,7 +72,7 @@ public class KvOperations {
     {
       // tag::get-simple[]
       try {
-        GetResult getResult = defaultCollection.get("document-key");
+        GetResult getResult = collection.get("document-key");
         String title = getResult.contentAsObject().getString("title");
         System.out.println(title); // title == "My Blog Post"
       } catch (DocumentNotFoundException ex) {
@@ -83,7 +83,7 @@ public class KvOperations {
 
     {
       // tag::get[]
-      GetResult found = defaultCollection.get("document-key");
+      GetResult found = collection.get("document-key");
       JsonObject content = found.contentAsObject();
       if (content.getString("author").equals("mike")) {
         // do something
@@ -95,26 +95,26 @@ public class KvOperations {
 
     {
       // tag::replace[]
-      defaultCollection.upsert("my-document", JsonObject.create().put("initial", true));
+      collection.upsert("my-document", JsonObject.create().put("initial", true));
 
-      GetResult result = defaultCollection.get("my-document");
+      GetResult result = collection.get("my-document");
       JsonObject content = result.contentAsObject();
       content.put("modified", true).put("initial", false);
-      defaultCollection.replace("my-document", content, replaceOptions().cas(result.cas()));
+      collection.replace("my-document", content, replaceOptions().cas(result.cas()));
       // end::replace[]
     }
 
     {
       // tag::replace-retry[]
       String id = "my-document";
-      defaultCollection.upsert(id, JsonObject.create().put("initial", true));
+      collection.upsert(id, JsonObject.create().put("initial", true));
 
-      GetResult found = defaultCollection.get(id);
+      GetResult found = collection.get(id);
       JsonObject content = found.contentAsObject();
       content.put("modified", true).put("initial", false);
       while (true) {
         try {
-          defaultCollection.replace(id, content, replaceOptions().cas(found.cas()));
+          collection.replace(id, content, replaceOptions().cas(found.cas()));
           break; // if successful, break out of the retry loop
         } catch (CasMismatchException ex) {
           // don't do anything, we'll retry the loop
@@ -126,7 +126,7 @@ public class KvOperations {
     {
       // tag::remove[]
       try {
-        defaultCollection.remove("my-document");
+        collection.remove("my-document");
       } catch (DocumentNotFoundException ex) {
         System.out.println("Document did not exist when trying to remove");
       }
@@ -136,7 +136,7 @@ public class KvOperations {
     {
       try {
         // tag::durability[]
-        defaultCollection.upsert("my-document", JsonObject.create().put("doc", true),
+        collection.upsert("my-document", JsonObject.create().put("doc", true),
             upsertOptions().durability(DurabilityLevel.MAJORITY));
         // end::durability[]
       } catch (DurabilityImpossibleException di) {
@@ -147,7 +147,7 @@ public class KvOperations {
     {
       try {
         // tag::durability-observed[]
-        defaultCollection.upsert("my-document", JsonObject.create().put("doc", true),
+        collection.upsert("my-document", JsonObject.create().put("doc", true),
             upsertOptions().durability(PersistTo.NONE, ReplicateTo.TWO));
         // end::durability-observed[]
       } catch (ReplicaNotConfiguredException rnc) {
@@ -157,43 +157,55 @@ public class KvOperations {
 
     {
       // tag::expiry-insert[]
-      MutationResult insertResult = defaultCollection.insert("my-document2", json, insertOptions().expiry(Duration.ofHours(2)));
+      MutationResult insertResult = collection.insert("my-document2", json,
+          insertOptions().expiry(Duration.ofHours(2)));
       // end::expiry-insert[]
     }
 
     {
       // tag::expiry-insert-instant[]
-      MutationResult insertResult = defaultCollection.insert("my-document3", json,
+      MutationResult insertResult = collection.insert("my-document3", json,
           insertOptions().expiry(Instant.now().plus(Period.ofDays(62))));
       // end::expiry-insert-instant[]
     }
 
     {
       // tag::expiry-get[]
-      GetResult found = defaultCollection.get("my-document3", getOptions().withExpiry(true));
+      GetResult found = collection.get("my-document3", getOptions().withExpiry(true));
       System.out.println("Expiry of found doc: " + found.expiry());
       // end::expiry-get[]
     }
 
     {
       // tag::expiry-replace[]
-      GetResult found = defaultCollection.get("my-document3", getOptions().withExpiry(true));
+      GetResult found = collection.get("my-document3", getOptions().withExpiry(true));
 
-      defaultCollection.replace("my-document3", json, replaceOptions().expiry(found.expiryTime().get()));
+      collection.replace("my-document3", json, replaceOptions().expiry(found.expiryTime().get()));
       // end::expiry-replace[]
     }
 
     {
       // tag::expiry-touch[]
-      defaultCollection.getAndTouch("my-document3", Duration.ofDays(1));
+      collection.getAndTouch("my-document3", Duration.ofDays(1));
       // end::expiry-touch[]
     }
 
-    cleanupData(defaultCollection);
+    {
+      // tag::named-collection-upsert[]
+      Scope inventoryScope = bucket.scope("inventory");
+      Collection airportCollection = inventoryScope.collection("airport");
+
+      JsonObject content = JsonObject.create().put("airportname", "Aeroporto D'Abruzzo").put("country", "Italy");
+      MutationResult result = airportCollection.upsert("airport-doc-key", content);
+      // end::named-collection-upsert[]
+      System.out.println("namedCollectionUpsert, cas value: " + result.cas());
+    }
+
+    // Cleans up example data from _default collection
+    // to avoid errors when running the sample code (mainly for `insert` examples).
+    cleanupData(collection);
   }
 
-  // Cleans up example data from a specified collection 
-  // to avoid errors when running the sample code (mainly for `insert` examples).
   private static void cleanupData(Collection collection) throws CouchbaseException {
     System.out.println("\nCleaning up example data....");
 
--- modules/howtos/examples/Queries.java
+++ modules/howtos/examples/Queries.java
@@ -17,21 +17,30 @@
  // NB: This example requires the `travel-sample` bucket to be installed.
 
 // tag::imports[]
-import com.couchbase.client.core.error.CouchbaseException;
-import com.couchbase.client.java.*;
-import com.couchbase.client.java.json.*;
-import com.couchbase.client.java.kv.MutationResult;
-import com.couchbase.client.java.kv.MutationState;
-import com.couchbase.client.java.query.*;
-import org.reactivestreams.Subscription;
-import reactor.core.publisher.*;
+import static com.couchbase.client.java.query.QueryOptions.queryOptions;
 
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static com.couchbase.client.java.query.QueryOptions.queryOptions;
-// end::imports[]
+import com.couchbase.client.core.error.CouchbaseException;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.Scope;
+import com.couchbase.client.java.json.JsonArray;
+import com.couchbase.client.java.json.JsonObject;
+import com.couchbase.client.java.kv.MutationResult;
+import com.couchbase.client.java.kv.MutationState;
+import com.couchbase.client.java.query.QueryOptions;
+import com.couchbase.client.java.query.QueryResult;
+import com.couchbase.client.java.query.QueryScanConsistency;
+import com.couchbase.client.java.query.ReactiveQueryResult;
 
+import org.reactivestreams.Subscription;
+
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Mono;
+// end::imports[]
 
 public class Queries {
 
@@ -41,15 +50,14 @@ public class Queries {
     {
       // tag::simple[]
       try {
-        final QueryResult result = cluster
-          .query("select * from `travel-sample` limit 10", queryOptions().metrics(true));
+        final QueryResult result = cluster.query("select * from `travel-sample` limit 10",
+            queryOptions().metrics(true));
 
         for (JsonObject row : result.rowsAsObject()) {
           System.out.println("Found row: " + row);
         }
 
-        System.out.println("Reported execution time: "
-          + result.metaData().metrics().get().executionTime());
+        System.out.println("Reported execution time: " + result.metaData().metrics().get().executionTime());
       } catch (CouchbaseException ex) {
         ex.printStackTrace();
       }
@@ -59,28 +67,24 @@ public class Queries {
     {
       // tag::named[]
       QueryResult result = cluster.query(
-        "select count(*) from `travel-sample` where type = \"airport\" and country = $country",
-        queryOptions().parameters(JsonObject.create().put("country", "France"))
-      );
+          "select count(*) from `travel-sample` where type = \"airport\" and country = $country",
+          queryOptions().parameters(JsonObject.create().put("country", "France")));
       // end::named[]
     }
-
 
     {
       // tag::positional[]
       QueryResult result = cluster.query(
-        "select count(*) from `travel-sample` where type = \"airport\" and country = ?",
-        queryOptions().parameters(JsonArray.from("France"))
-      );
+          "select count(*) from `travel-sample` where type = \"airport\" and country = ?",
+          queryOptions().parameters(JsonArray.from("France")));
       // end::positional[]
     }
 
     {
       // tag::scanconsistency[]
       QueryResult result = cluster.query(
-        "select count(*) from `travel-sample`",
-        queryOptions().scanConsistency(QueryScanConsistency.REQUEST_PLUS)
-      );
+          "select count(*) from `travel-sample` where type = \"airport\" and country = 'France'",
+          queryOptions().scanConsistency(QueryScanConsistency.REQUEST_PLUS));
       // end::scanconsistency[]
     }
 
@@ -88,48 +92,40 @@ public class Queries {
       // tag::scanconsistency_with[]
       Bucket bucket = cluster.bucket("travel-sample");
       Collection collection = bucket.defaultCollection();
-      MutationResult mr = collection.upsert("someDoc",JsonObject.create().put("name", "roi"));
+      MutationResult mr = collection.upsert("someDoc", JsonObject.create().put("name", "roi"));
       MutationState mutationState = MutationState.from(mr.mutationToken().get());
 
       QueryOptions qo = QueryOptions.queryOptions().consistentWith(mutationState);
-      QueryResult result = cluster.query(
-        "select raw meta().id from `travel-sample` limit 100;", qo
-      );
+      QueryResult result = cluster.query("select raw meta().id from `travel-sample` limit 100;", qo);
       // end::scanconsistency_with[]
     }
 
     {
       // tag::clientcontextid[]
       QueryResult result = cluster.query(
-        "select count(*) from `travel-sample`",
-        queryOptions().clientContextId("user-44" + UUID.randomUUID())
-      );
+          "select count(*) from `travel-sample` where type = \"airport\" and country = 'France'",
+          queryOptions().clientContextId("user-44" + UUID.randomUUID()));
       // end::clientcontextid[]
     }
 
     {
       // tag::readonly[]
       QueryResult result = cluster.query(
-        "select count(*) from `travel-sample`",
-        queryOptions().readonly(true)
-      );
+          "select count(*) from `travel-sample` where type = \"airport\" and country = 'France'",
+          queryOptions().readonly(true));
       // end::readonly[]
     }
 
     {
       // tag::printmetrics[]
       QueryResult result = cluster.query("select 1=1", queryOptions().metrics(true));
-      System.err.println(
-        "Execution time: " + result.metaData().metrics().get().executionTime()
-      );
+      System.err.println("Execution time: " + result.metaData().metrics().get().executionTime());
       // end::printmetrics[]
     }
 
     {
       // tag::rowsasobject[]
-      QueryResult result = cluster.query(
-        "select * from `travel-sample` limit 10"
-      );
+      QueryResult result = cluster.query("select * from `travel-sample` limit 10");
       for (JsonObject row : result.rowsAsObject()) {
         System.out.println("Found row: " + row);
       }
@@ -138,43 +134,49 @@ public class Queries {
 
     {
       // tag::simplereactive[]
-      Mono<ReactiveQueryResult> result = cluster
-        .reactive()
-        .query("select 1=1");
+      Mono<ReactiveQueryResult> result = cluster.reactive().query("select 1=1");
 
-      result
-        .flatMapMany(ReactiveQueryResult::rowsAsObject)
-        .subscribe(row -> System.out.println("Found row: " + row));
+      result.flatMapMany(ReactiveQueryResult::rowsAsObject).subscribe(row -> System.out.println("Found row: " + row));
       // end::simplereactive[]
     }
 
     {
       // tag::backpressure[]
-      Mono<ReactiveQueryResult> result = cluster
-        .reactive()
-        .query("select * from hugeBucket");
+      Mono<ReactiveQueryResult> result = cluster.reactive().query("select * from `travel-sample`");
 
-      result
-        .flatMapMany(ReactiveQueryResult::rowsAsObject)
-        .subscribe(new BaseSubscriber<JsonObject>() {
-          // Number of outstanding requests
-          final AtomicInteger oustanding = new AtomicInteger(0);
+      result.flatMapMany(ReactiveQueryResult::rowsAsObject).subscribe(new BaseSubscriber<JsonObject>() {
+        // Number of outstanding requests
+        final AtomicInteger oustanding = new AtomicInteger(0);
 
-          @Override
-          protected void hookOnSubscribe(Subscription subscription) {
-            request(10); // initially request to rows
-            oustanding.set(10);
+        @Override
+        protected void hookOnSubscribe(Subscription subscription) {
+          request(10); // initially request to rows
+          oustanding.set(10);
+        }
+
+        @Override
+        protected void hookOnNext(JsonObject value) {
+          process(value);
+          if (oustanding.decrementAndGet() == 0) {
+            request(10);
           }
-
-          @Override
-          protected void hookOnNext(JsonObject value) {
-            process(value);
-            if (oustanding.decrementAndGet() == 0) {
-              request(10);
-            }
-          }
-        });
+        }
+      });
       // end::backpressure[]
+    }
+
+    {
+      // tag::inventory-scope-query[]
+      Bucket bucket = cluster.bucket("travel-sample");
+      Scope scope = bucket.scope("inventory");
+
+      QueryResult result = scope.query("select * from `airline` where country = $country LIMIT 10",
+          queryOptions().parameters(JsonObject.create().put("country", "France")));
+
+      for (JsonObject row : result.rowsAsObject()) {
+        System.out.println("Found row: " + row);
+      }
+      // end::inventory-scope-query[]
     }
 
   }
