20aaa3fd4d089c541fa096d4a4a4c1c9fcbf5a55
Maria Shodunke <mariashow2001@gmail.com>
2022-03-30 11:45:54 +0100

    DOC-9567 - Add scoped index management examples
    - Use changes made in this sdk-common PR:
      https://github.com/couchbase/docs-sdk-common/pull/47
    
    - Add Index management examples with scopes and collections (new in 3.3)
    
    - Fills in examples that were missing/commented out, apart from Views.
      Let me know if I should add some views examples in though,
      I just thought we generally don't want to mention too much about them in the docs,
      assuming that they will be gone in some distant future...

---
+++ modules/howtos/examples/ProvisioningResourcesBuckets.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2022 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import com.couchbase.client.core.error.BucketExistsException;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.manager.bucket.BucketManager;
+import com.couchbase.client.java.manager.bucket.BucketSettings;
+import com.couchbase.client.java.manager.bucket.BucketType;
+import com.couchbase.client.java.manager.bucket.ConflictResolutionType;
+
+public class ProvisioningResourcesBuckets {
+	public static void main(String[] args) {
+		// tag::creatingbucketmgr[]
+		Cluster cluster = Cluster.connect("localhost", "Administrator", "password");
+		BucketManager bucketMgr = cluster.buckets();
+		// end::creatingbucketmgr[]
+
+		{
+			System.out.println("[createBucket]");
+
+			// tag::createBucket[]
+			try {
+				BucketSettings bucketSettings = BucketSettings.create("hello")
+						.flushEnabled(false)
+						.replicaIndexes(true)
+						.ramQuotaMB(150)
+						.numReplicas(1)
+						.bucketType(BucketType.COUCHBASE)
+						.conflictResolutionType(ConflictResolutionType.SEQUENCE_NUMBER);
+
+				bucketMgr.createBucket(bucketSettings);
+			} catch (BucketExistsException e) {
+				System.out.println("Bucket already exists");
+			}
+			// end::createBucket[]
+		}
+
+		{
+			System.out.println("[updateBucket]");
+
+			// tag::updateBucket[]
+			BucketSettings settings = bucketMgr.getBucket("hello");
+			settings.flushEnabled(true);
+
+			bucketMgr.updateBucket(settings);
+			// end::updateBucket[]
+		}
+		{
+			System.out.println("[flushBucket]");
+
+			// tag::flushBucket[]
+			bucketMgr.flushBucket("hello");
+			// end::flushBucket[]
+		}
+		{
+			System.out.println("[removeBucket]");
+
+			// tag::removeBucket[]
+			bucketMgr.dropBucket("hello");
+			// end::removeBucket[]
+		}
+	}
+}
